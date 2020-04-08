8f1821cb28cd7504a016a23b696981942855fbe5
Jhon Vargas <jhevargasv@correo.udistrital.edu.co>
2020-04-08 16:40:28 -0500

    Aleatorio sobre la lista WORD

--- Python Ahorcado.py
+++ Python Ahorcado.py
@@ -1,3 +1,5 @@
+import random
+
 # se define una constante con mayusculas 
 IMAGES = ['''
 
@@ -73,3 +75,18 @@ IMAGES = ['''
         =========''', '''
 
 ''']
+
+WORD=['lavadora','secadora','sofa','democracia','diputado','teclado']
+
+
+def main():
+    word = rando_word()
+
+    pass
+
+def rando_word():
+    idx = randon.ranint(0, len(WORD)-1)
+
+if __name__ == '__main__':
+    print('B I EN V E N I D O S  A L  J U E G O  A H O R C A D O S')
+    main()
